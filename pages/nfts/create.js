e6494295894ccc20f94498c7c5a1fbf2259d34cd
AdeelK-tech <adeeltech12@gmail.com>
2022-05-06 14:53:17 +0500

    some css

--- pages/nfts/create.js
+++ pages/nfts/create.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { Form, Input, Label } from "semantic-ui-react";
+import { Form, Input,Grid} from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import Image from "next/image";
 const createNFT = () => {
   return (
     <Layout>
       <h2>Create an NFT</h2>
+      <Grid colunms={2}>
+        <Grid.Row>
+        <Grid.Column width={8}>
       <Form>
         <Form.Field>
           <label style={{ fontSize: "large" }}>Name</label>
-          <Input></Input>
+          <Input style={{width:'500px'}}></Input>
+        </Form.Field>
+        <Form.Field>
+          <label style={{ fontSize: "large" }}>Name</label>
+          <Input style={{width:'500px'}}></Input>
+        </Form.Field>
+        <Form.Field>
+          <label style={{ fontSize: "large" }}>Name</label>
+          <Input style={{width:'500px'}}></Input>
         </Form.Field>
       </Form>
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Image src='/image_1.png' width={`${500}px`} height={`${500}px`}></Image>
+      </Grid.Column>
+      </Grid.Row>
+      </Grid>
     </Layout>
   );
 };
